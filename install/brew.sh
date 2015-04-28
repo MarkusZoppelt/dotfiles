a064bb0078eae064636bbf8c66266e9d4a6cd18b
MarkusZoppelt <markus@zoppelt.net>
2015-04-28 19:18:28 +0200

    Syntastic only enabled in Airline

--- install/brew.sh
+++ install/brew.sh
@@ -8,21 +8,20 @@ brew install ffmpeg
 brew install git
 brew install glassfish
 brew install imagemagick
+brew install macvim --override-system-vim
 brew install irssi
 brew install markdown
 brew install maven
-# brew install node
 brew install nvm
 brew install noweb
-brew install openssl
+# brew install openssl
 brew install peco
-brew install qt
+# brew install qt
 brew install reattach-to-user-namespace
 brew install speedtest_cli
 brew install tmux
 brew install tree
 brew install v8
-brew install vim
 brew install wget
 brew install youtube-dl
 brew install zsh

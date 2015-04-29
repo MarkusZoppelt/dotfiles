61ae7b869d8d8512ac708441875683d70d59b624
MarkusZoppelt <markus@zoppelt.net>
2015-04-30 01:02:17 +0200

    kj does <ESC>, tmux aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,3 +11,9 @@ alias master=_master
 alias blogupdate="cd ~/blog ; jekyll build && scp -r _site/* mzoppelt@sagitta.uberspace.de:html/ "
 alias bloglocal="cd ~/blog && jekyll serve --config _config_dev.yml -w"
 alias pi="ssh pi@192.168.2.116"
+
+# tmux resizing
+alias tdown="tmux resize-pane -D $1"
+alias tright="tmux resize-pane -R $1"
+alias tleft="tmux resize-pane -L $1"
+alias tup="tmux resize-pane -U $1"

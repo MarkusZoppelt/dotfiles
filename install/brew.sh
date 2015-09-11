53294628344899bc6282a284c468aea5bff16690
MarkusZoppelt <markus@zoppelt.net>
2015-09-12 00:51:54 +0200

    Updated brews

--- install/brew.sh
+++ install/brew.sh
@@ -3,7 +3,7 @@
 
 brew install aria2
 brew install cmake
-brew install coq
+# brew install coq
 brew install ffmpeg
 brew install git
 brew install glassfish
@@ -13,7 +13,7 @@ brew install irssi
 brew install markdown
 brew install maven
 brew install nvm
-brew install noweb
+# brew install noweb
 # brew install openssl
 brew install peco
 # brew install qt
@@ -25,3 +25,4 @@ brew install v8
 brew install wget
 brew install youtube-dl
 brew install zsh
+brew tap beeftornado/rmtree && brew install beeftornado/rmtree/brew-rmtree

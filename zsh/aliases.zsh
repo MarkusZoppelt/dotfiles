3bde649c9dac99c7b4790f6ba430cb8ece08cfc4
MarkusZoppelt <markus@zoppelt.net>
2016-03-23 07:01:37 +0100

    minor changes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,13 +1,5 @@
 alias ll="ls -l"
 alias uberspace="ssh mzoppelt@sagitta.uberspace.de"
-alias vpn="osascript ~/Scripts/VPN-Campus.scpt"
-alias novpn="osascript ~/Scripts/NO-VPN-Campus.scpt"
-_master()
-{
-	cd /Users/markuszoppelt/Studium/Master/1-SS2015
-	cd $1
-}
-alias master=_master
 alias blogupdate="cd ~/blog ; jekyll build && scp -r _site/* mzoppelt@zoppelt.net:html/ "
 alias blogupdate-lite="cd ~/blog ; jekyll build && scp -r _site/*[^assets/] mzoppelt@zoppelt.net:html/ "
 alias bloglocal="cd ~/blog && jekyll serve --config _config_dev.yml -w"

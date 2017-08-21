13eab851510c22155c0d3e7137ae48c071cff38e
Markus Zoppelt <markus.zoppelt@th-nuernberg.de>
2017-08-22 00:24:56 +0200

    even less is even more

--- install/brew.sh
+++ install/brew.sh
@@ -4,11 +4,9 @@
 brew install aria2
 brew install ffmpeg --with-sdl2
 brew install git
-brew install git-lfs
 brew install hub
 brew install imagemagick
-# brew install macvim --with-override-system-vim
-brew install neovim/neovim/neovim
+brew install vim
 brew install reattach-to-user-namespace
 brew install beeftornado/rmtree/brew-rmtree
 brew install tmux

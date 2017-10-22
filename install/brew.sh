8496f75646a7c595190dfad0363e6188a67d87ae
Markus Zoppelt <markus.zoppelt@th-nuernberg.de>
2017-10-22 21:59:36 +0200

    minor changes

--- install/brew.sh
+++ install/brew.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# give me what I need, brew!
 
 brew install aria2
 brew install ffmpeg --with-sdl2
@@ -12,6 +11,3 @@ brew install beeftornado/rmtree/brew-rmtree
 brew install tmux
 brew install wget
 brew install youtube-dl
-
-# brew cask
-brew install caskroom/cask/brew-cask

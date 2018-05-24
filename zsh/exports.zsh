d6c9032e941e1ba6d4e03d4b8936b7477b2f9d80
Markus Zoppelt <markus.zoppelt@th-nuernberg.de>
2018-05-24 09:54:39 +0200

    added sbin to path

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -21,4 +21,8 @@ if [[ -d /usr/local/bin ]]; then
 	export PATH=/usr/local/bin:$PATH
 fi
 
+if [[ -d /usr/local/sbin ]]; then
+	export PATH=/usr/local/sbin:$PATH
+fi
+
 export MANPATH="/usr/local/man:$MANPATH"

c6d384bc2d7123d36c0fa64264cfa5b187eb1096
Markus Zoppelt <markus@zoppelt.net>
2022-09-10 15:51:42 +0200

    minor additions/changes

--- nvim/init.vim
+++ nvim/init.vim
@@ -21,15 +21,30 @@ set t_Co=256
 set t_ut=
 colorscheme codedark
 
+" Load Lua scripts
+lua require('lang.go')
+lua require('lang.rust')
+
+autocmd BufWritePre *.go :silent! lua require('go.format').gofmt()
+
+set completeopt=menu,menuone,noselect
+
+" Code navigation shortcuts
+nnoremap <silent> K     <cmd>lua vim.lsp.buf.hover()<CR>
+nnoremap <silent> gD    <cmd>lua vim.lsp.buf.implementation()<CR>
+nnoremap <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
+nnoremap <silent> 1gD   <cmd>lua vim.lsp.buf.type_definition()<CR>
+nnoremap <silent> gr    <cmd>lua vim.lsp.buf.references()<CR>
+nnoremap <silent> g0    <cmd>lua vim.lsp.buf.document_symbol()<CR>
+nnoremap <silent> gW    <cmd>lua vim.lsp.buf.workspace_symbol()<CR>
+nnoremap <silent> gd    <cmd>lua vim.lsp.buf.definition()<CR>
+nnoremap <silent> ga    <cmd>lua vim.lsp.buf.code_action()<CR>
 " Telescope
 nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
 nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
 nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
 nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
 
-autocmd BufWritePre *.go :silent! lua require('go.format').gofmt()
-
-set completeopt=menu,menuone,noselect
-
-lua require('lang.go')
-lua require('lang.rust')
+" Set updatetime for CursorHold
+" 300ms of no cursor movement to trigger CursorHold
+set updatetime=300
--- nvim/lua/lang/rust.lua
+++ nvim/lua/lang/rust.lua
@@ -1,6 +1,5 @@
 -- Rust Setup
-
-local nvim_lsp = require'lspconfig'
+local nvim_lsp = require 'lspconfig'
 
 local opts = {
     tools = { -- rust-tools options
@@ -8,52 +7,44 @@ local opts = {
         inlay_hints = {
             show_parameter_hints = false,
             parameter_hints_prefix = "",
-            other_hints_prefix = "",
-        },
+            other_hints_prefix = ""
+        }
     },
     server = {
         settings = {
             ["rust-analyzer"] = {
                 -- enable clippy on save
-                checkOnSave = {
-                    command = "clippy"
-                },
+                checkOnSave = {command = "clippy"}
             }
         }
-    },
+    }
 }
 
 require('rust-tools').setup(opts)
 
-local cmp = require'cmp'
+local cmp = require 'cmp'
 cmp.setup({
-  -- Enable LSP snippets
-  snippet = {
-    expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    -- Add tab support
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    ['<Tab>'] = cmp.mapping.select_next_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
-    })
-  },
+    -- Enable LSP snippets
+    snippet = {expand = function(args) vim.fn["vsnip#anonymous"](args.body) end},
+    mapping = {
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        -- Add tab support
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true
+        })
+    },
 
-  -- Installed sources
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'vsnip' },
-    { name = 'path' },
-    { name = 'buffer' },
-  },
+    -- Installed sources
+    sources = {
+        {name = 'nvim_lsp'}, {name = 'vsnip'}, {name = 'path'},
+        {name = 'buffer'}
+    }
 })

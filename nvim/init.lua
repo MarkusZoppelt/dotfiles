73561ea500e04590914cbb8395c1a899f40d4cc2
Markus Zoppelt <markus.zoppelt@code-intelligence.com>
2022-11-25 13:46:07 +0100

    feat: add rounded corners to vim floating windows

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,7 @@
 local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 Plug 'github/copilot.vim'
--- Plug 'gruvbox-community/gruvbox'
 Plug('folke/tokyonight.nvim', {branch = 'main'})
--- Plug 'nvim-tree/nvim-web-devicons'
--- Plug 'kyazdani42/nvim-tree.lua'
--- Plug 'nvim-lualine/lualine.nvim'
 Plug 'neovim/nvim-lspconfig'
 Plug 'nvim-lua/plenary.nvim'
 Plug('nvim-telescope/telescope.nvim', { branch = '0.1.x' })
@@ -26,8 +22,6 @@ vim.call('plug#end')
 -- set term color 256
 vim.o.termguicolors = true
 
--- vim.g.loaded = 1
--- vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = ","
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -45,18 +39,22 @@ vim.opt.linebreak = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.completeopt = "menu,menuone,noselect"
 
--- require("nvim-tree").setup()
 require('tokyonight').setup({transparent = true})
 require('lspconfig').clangd.setup{}
 require('lspconfig').gopls.setup{}
 require('lspconfig').rust_analyzer.setup{}
--- require('lualine').setup()
 require('lang.go')
 require('lang.rust')
 require('treesitter')
 require('treesitter-context').setup()
 require('copilot')
 require('remaps')
+
+-- Rounded borders for floating windows
+require('cmp').setup({ window = { completion = {border = 'rounded'} } })
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {border = 'rounded'})
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {border = 'rounded'})
+vim.diagnostic.config{ float= {border='rounded'} }
 
 vim.cmd [[
   hi Normal guibg=NONE ctermbg=NONE

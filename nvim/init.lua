ca16d9030a2101158a4f9dc30b250367d04fb28d
Markus Zoppelt <markus@zoppelt.net>
2022-12-23 17:15:45 +0100

    feat: add colorcolumn 80 for nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,6 +29,7 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.colorcolumn = "80"
 vim.opt.backspace = "indent,eol,start"
 vim.opt.mouse = "a"
 vim.opt.autoindent = true

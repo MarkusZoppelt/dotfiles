fadbb148c7131716fb7cd2ea5608da25f57cf685
Markus Zoppelt <markus.zoppelt@code-intelligence.com>
2023-04-02 22:32:13 +0200

    feat: add javascript to lua/lang

--- nvim/lua/lang/typescript.lua
+++ nvim/lua/lang/typescript.lua
@@ -17,6 +17,6 @@ end
 -- TypeScript
 nvim_lsp.tsserver.setup {
   on_attach = on_attach,
-  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+  filetypes = { "javascript", "typescript", "typescriptreact", "typescript.tsx" },
   cmd = { "typescript-language-server", "--stdio" }
 }

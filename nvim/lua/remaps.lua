63650c3be9a74ac363a30b74b018f5705121d912
Markus Zoppelt <markus@zoppelt.net>
2023-08-22 00:00:32 +0200

    feat(nvim): clever remaps
    e.g., move lines up or down in visual mode with 'J' / 'K'

--- nvim/lua/remaps.lua
+++ nvim/lua/remaps.lua
@@ -14,3 +14,13 @@ vim.keymap.set('n', 'ff', builtin.find_files, {})
 vim.keymap.set('n', 'fg', builtin.live_grep, {})
 vim.keymap.set('n', 'fb', builtin.buffers, {})
 vim.keymap.set('n', 'fh', builtin.help_tags, {})
+
+-- Move lines in visual mode
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+vim.keymap.set('x', '<leader>p"', '"_dP')
+
+-- Set leader y to copy to system clipboard
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')

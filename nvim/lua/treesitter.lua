f07e7d6ade67d2a12a3f2f3610ead0282f336ba1
Markus Zoppelt <markus@zoppelt.net>
2023-08-21 23:06:35 +0200

    fix(nvim): set vim regex highlighting to false
    ... because of performance reasons.
    treesitter does everything on its own

--- nvim/lua/treesitter.lua
+++ nvim/lua/treesitter.lua
@@ -11,6 +11,6 @@ require 'nvim-treesitter.configs'.setup {
     highlight = {
         -- `false` will disable the whole extension
         enable = true,
-        additional_vim_regex_highlighting = true,
+        additional_vim_regex_highlighting = false,
     },
 }

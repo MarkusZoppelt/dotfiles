4f3be3cbf2e725b90470a8be16f4dc72fba0e11c
Markus Zoppelt <markus@zoppelt.net>
2024-01-16 10:13:01 +0100

    feat(wezterm): nice (macOS) fullscreen

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,19 @@
 local wezterm = require 'wezterm'
 local config = {
     hide_tab_bar_if_only_one_tab = true,
+    native_macos_fullscreen_mode = false,
 }
 
 config.color_scheme = 'Tokyo Night'
 config.font = wezterm.font 'Monaspace Neon'
 config.font_size = 16
+
+config.keys = {
+  {
+    key = 'n',
+    mods = 'SHIFT|CTRL',
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
 
 return config

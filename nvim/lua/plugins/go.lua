e2a6f505d8f83f751726ff513b04ec104c7c85b4
Markus Zoppelt <markus@zoppelt.net>
2024-03-28 09:07:51 +0100

    feat(nvim): update go.nvim config & plugins

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -1,28 +1,23 @@
 return {
-    "ray-x/go.nvim",
-    dependencies = {
-        "ray-x/guihua.lua",
-        "neovim/nvim-lspconfig",
-        "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-        require("go").setup({
-            goimport = 'gopls',
-            fillstruct = 'gopls',
-            gofmt = 'gopls',
-            max_line_len = 120
-        })
-
-        -- Run gofmt + goimport on save
-        local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = "*.go",
-            callback = function()
-                require('go.format').goimport()
-            end,
-            group = format_sync_grp,
-        })
-    end,
-    ft = { "go", 'gomod' },
-    build = ':lua require("go.install").update_all_sync()'
+  "ray-x/go.nvim",
+  dependencies = {
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("go").setup()
+    -- Run gofmt + goimport on save
+    local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.go",
+      callback = function()
+       require('go.format').goimports()
+      end,
+      group = format_sync_grp,
+    })
+  end,
+  event = {"CmdlineEnter"},
+  ft = {"go", 'gomod'},
+  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
 }

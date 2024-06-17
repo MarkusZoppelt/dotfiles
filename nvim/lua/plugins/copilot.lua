9c8902f11ca9421b444931653f2c34a4947d18a4
Markus Zoppelt <markus@zoppelt.net>
2024-06-17 10:16:50 +0200

    fix(nvim): start copilot without event
    ... because it didn't start reliably with lazy events

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,6 +1,5 @@
 return {
     "github/copilot.vim",
-    event = { "InsertEnter", "CmdLineEnter"},
     config = function()
         vim.g.copilot_no_tab_map = true
         vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })

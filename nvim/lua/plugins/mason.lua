e7e0aa719f915a1e1c119c1f9b3b733631d56d86
Markus Zoppelt <markus@zoppelt.net>
2024-09-09 11:56:54 +0200

    fix(nvim): replace tsserver with ts_ls (mason)
    ... tsserver is deprecated

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -9,9 +9,9 @@ return {
         local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
         require('mason-lspconfig').setup({
             ensure_installed = {
-                'tsserver',
                 'gopls',
                 'rust_analyzer',
+                'ts_ls',
             }
         })
         require('mason-lspconfig').setup_handlers({

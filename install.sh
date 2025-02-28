cf2443a4e5c8bf4a20ecf5aa17ff50cd96dc084b
Markus Zoppelt <markus@zoppelt.net>
2025-02-28 10:49:29 +0100

    chore: update & format install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 echo "Creating symlinks"
-ln -nsf ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -nsf ~/.dotfiles/.gitignore_global ~/.gitignore_global
-ln -nsf ~/.dotfiles/.profile ~/.profile
-ln -nsf ~/.dotfiles/.tmux.conf ~/.tmux.conf
-ln -nsf ~/.dotfiles/.zshrc ~/.zshrc
+ln -nsf ~/.dotfiles/.gitconfig           ~/.gitconfig
+ln -nsf ~/.dotfiles/.gitignore_global    ~/.gitignore_global
+ln -nsf ~/.dotfiles/.profile             ~/.profile
+ln -nsf ~/.dotfiles/.tmux.conf           ~/.tmux.conf
+ln -nsf ~/.dotfiles/.zshrc               ~/.zshrc
+
 mkdir -p ~/.config
-ln -nsf ~/.dotfiles/config/alacritty ~/.config/alacritty
-ln -nsf ~/.dotfiles/config/gh-copilot ~/.config/gh-copilot
-ln -nsf ~/.dotfiles/config/ghostty ~/.config/ghostty
-ln -nsf ~/.dotfiles/config/nvim ~/.config/nvim
+ln -nsf ~/.dotfiles/config/alacritty     ~/.config/alacritty
+ln -nsf ~/.dotfiles/config/gh-copilot    ~/.config/gh-copilot
+ln -nsf ~/.dotfiles/config/ghostty       ~/.config/ghostty
+ln -nsf ~/.dotfiles/config/lazygit       ~/.config/lazygit
+ln -nsf ~/.dotfiles/config/nvim          ~/.config/nvim
 ln -nsf ~/.dotfiles/config/starship.toml ~/.config/starship.toml
-ln -nsf ~/.dotfiles/config/wezterm ~/.config/wezterm
+ln -nsf ~/.dotfiles/config/wezterm       ~/.config/wezterm
 
 if [ "$(uname)" == "Darwin" ]; then
     brew bundle --file ~/.dotfiles/Brewfile
     ln -nsf ~/.dotfiles/.zprofile ~/.zprofile
-    ln -nsf ~/.dotfiles/.zshenv ~/.zshenv
+    ln -nsf ~/.dotfiles/.zshenv   ~/.zshenv
 fi
 
 # Installing tools

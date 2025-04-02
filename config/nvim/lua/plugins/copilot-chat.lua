94a16903131794d80067fb1b5bb20fd84ce4554c
Markus Zoppelt <markus@zoppelt.net>
2025-04-02 11:55:50 +0200

    feat(nvim): add oil.nvim and CopilotChat.nvim plugins

---
+++ config/nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" },
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+    },
+  },
+}

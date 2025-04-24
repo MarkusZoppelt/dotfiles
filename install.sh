c89c945f1122c67e1dbe05385ea0df6615db84f9
Markus Zoppelt <markus@zoppelt.net>
2025-04-24 10:37:06 +0200

    chore: remove alacritty and wezterm config
    ... in favor of Ghostty

--- install.sh
+++ install.sh
@@ -8,13 +8,11 @@ ln -nsf ~/.dotfiles/.tmux.conf           ~/.tmux.conf
 ln -nsf ~/.dotfiles/.zshrc               ~/.zshrc
 
 mkdir -p ~/.config
-ln -nsf ~/.dotfiles/config/alacritty     ~/.config/alacritty
 ln -nsf ~/.dotfiles/config/gh-copilot    ~/.config/gh-copilot
 ln -nsf ~/.dotfiles/config/ghostty       ~/.config/ghostty
 ln -nsf ~/.dotfiles/config/lazygit       ~/.config/lazygit
 ln -nsf ~/.dotfiles/config/nvim          ~/.config/nvim
 ln -nsf ~/.dotfiles/config/starship.toml ~/.config/starship.toml
-ln -nsf ~/.dotfiles/config/wezterm       ~/.config/wezterm
 
 if [ "$(uname)" == "Darwin" ]; then
     brew bundle --file ~/.dotfiles/Brewfile

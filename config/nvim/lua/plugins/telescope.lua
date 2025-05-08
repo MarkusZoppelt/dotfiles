0cf756a95b5f4f19d9dd1e33ce7dd26852ab649d
Markus Zoppelt <markus@zoppelt.net>
2025-05-08 14:41:35 +0200

    feat(nvim): add telescope diagnostics keymap

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', 'ff', builtin.find_files, {})
         vim.keymap.set('n', 'fg', builtin.live_grep, {})
+        vim.keymap.set('n', 'fd', builtin.diagnostics, {})
         vim.keymap.set('n', 'fb', builtin.buffers, {})
         vim.keymap.set('n', 'fh', builtin.help_tags, {})
     end

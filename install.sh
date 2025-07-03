ef442e92ce0c99b71e489b7224198ccc76c152a4
Markus Zoppelt <markus@zoppelt.net>
2025-07-03 17:35:57 +0200

    feat: add mako config

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ fi
 
 if command -v hyprland >/dev/null 2>&1; then
     ln -nsf ~/.dotfiles/config/hypr      ~/.config/hypr
+    ln -nsf ~/.dotfiles/config/mako      ~/.config/mako
     ln -nsf ~/.dotfiles/config/waybar    ~/.config/waybar
     ln -nsf ~/.dotfiles/config/wofi      ~/.config/wofi
 fi

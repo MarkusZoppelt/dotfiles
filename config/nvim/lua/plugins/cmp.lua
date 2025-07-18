2813231118a210bdc78fc02e7f981beb8606e132
Markus Zoppelt <markus@zoppelt.net>
2025-07-18 11:11:59 +0200

    perf: remove LuaSnip (never use it)

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -4,19 +4,12 @@ return {
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
-        "L3MON4D3/LuaSnip",
     },
     config = function()
         local cmp = require('cmp')
         cmp.setup({
             sources = {
                 { name = 'nvim_lsp' },
-                { name = 'luasnip' },
-            },
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body)
-                end,
             },
             mapping = {
                 ['<CR>'] = cmp.mapping.confirm({ select = false }),

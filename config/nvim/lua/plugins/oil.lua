8810f4e5a06da271678371de7a0b799f1aedbf36
Markus Zoppelt <markus@zoppelt.net>
2025-07-18 11:12:10 +0200

    perf: remove oil.nvim

--- config/nvim/lua/plugins/oil.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {},
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    lazy = false,
-}

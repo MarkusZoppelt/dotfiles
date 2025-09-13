8f7f4f824a991b339bdf379b32bd35def50d5fd8
Markus Zoppelt <markus@zoppelt.net>
2025-09-13 16:26:54 +0200

    feat(nvim): remove copilot

--- config/nvim/lua/plugins/copilot.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "github/copilot.vim",
-    config = function()
-        vim.g.copilot_no_tab_map = true
-        vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-    end
-}

d84069c0d45563e8cec6ebe2370ae3079ded55ea
Markus Zoppelt <markus@zoppelt.net>
2025-09-18 20:36:59 +0200

    feat: disable treesitter-context on launch

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -18,7 +18,7 @@ return {
     {
         "nvim-treesitter/nvim-treesitter-context",
         config = function()
-            require 'treesitter-context'.setup { enable = true }
+            require 'treesitter-context'.setup { enable = false }
         end
     }
 }
